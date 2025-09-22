8dc8c11231c8b71b0d3c45d86710c41ae08ba469
Nirajpandit19 <142142403+Nirajpandit19@users.noreply.github.com>
2025-09-22 21:52:25 +0530

    Update Greeter.java

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -4,4 +4,7 @@ public class Greeter {
     public String sayHello() {
         return "DevOps Project for AchiStar Technologies";
     }
+    public String sayHey() {
+        return "How are you bro";
+    }
 }
